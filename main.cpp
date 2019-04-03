f2fee5234bf1ece5bc51308b9084c17eef2f6c62
Hlib Kovalev <kovalev.mine@mail.ru>
2019-04-03 10:46:28 +0700

    Finished worked

--- main.cpp
+++ main.cpp
@@ -11,3 +11,20 @@ char board[8][8] = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
                     {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
                     {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
                     {'r', 'n', 'b', 'k', 'q', 'b', 'n', 'r'}};
+void printboard() {
+  int i, j;
+  for (i = 0; i < 8; i++) {
+    cout << numbers[i]<<' ';
+    for (j = 0; j < 8; j++) {
+      cout << board[i][j]<<' ';
+    }
+    cout << endl;
+  }
+  cout << "  ";
+  for (i = 1; i < 9; i++)
+    cout << i << ' ';
+}
+int main() {
+  printboard();
+  return 0;
+}
